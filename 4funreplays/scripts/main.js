0a40339a062fcf93ada552d2a825184aca987df8
Luis Eduardo <luis10barbo2020@gmail.com>
2022-02-10 00:27:41 -0300

    Added Night Mode Functionality

--- 4funreplays/scripts/main.js
+++ 4funreplays/scripts/main.js
@@ -1,6 +1,17 @@
 window.onload = () => {
+  const body = document.querySelector("body");
+  // Left Menu Functions
+
   const leftMenuNode = document.querySelector("#left-menu");
+  const lightModeButtonNode = document.querySelector("#night-mode-button");
+  const lightModeIcon = lightModeButtonNode.querySelector("i");
   document.querySelector("#left-menu-switch").addEventListener("click", () => {
-    leftMenuNode.classList.toggle("closed");
+    leftMenuNode.classList.toggle("open");
+  });
+
+  lightModeButtonNode.addEventListener("click", () => {
+    body.classList.toggle("light-mode");
+    lightModeIcon.classList.toggle("bxs-moon");
+    lightModeIcon.classList.toggle("bxs-sun");
   });
 };
